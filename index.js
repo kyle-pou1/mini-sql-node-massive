a0d1f56a0a7079f4f1ea8e7a6e32d54a4510f3f6
Macbook <kyle.poul@gmail.com>
2017-02-23 14:15:19 -0700

    completed tutorial

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var controller = require('./controller.js');
+
+var connectionString = 'postgres://kylepoulsen@localhost/sandbox';
+var massiveInstance = massive.connectSync({connectionString : connectionString})
+
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
+app.set('db', massiveInstance);
 
+var db = app.get('db');
+
+// db.new_plane(function(err, planes){
+//     console.log(err, "plane added")
+// });
+
+// db.get_planes(function(err, planes) {
+//   console.log(err, planes);
+// })
+
+controller.getPlanes();
+
+
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000")
+})
